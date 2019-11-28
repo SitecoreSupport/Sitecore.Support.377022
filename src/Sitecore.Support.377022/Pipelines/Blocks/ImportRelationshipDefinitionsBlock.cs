805e0467b7a513b091f0a54e4755f38210376bfe
mao <mao@sitecore.net>
2019-11-28 10:51:53 +0200

    Add default implementatation for ImportRelationshipDefinitionsBlock

---
+++ src/Sitecore.Support.377022/Pipelines/Blocks/ImportRelationshipDefinitionsBlock.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Framework.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.Blocks
+{
+    public class ImportRelationshipDefinitionsBlock : Sitecore.Commerce.Plugin.Catalog.ImportRelationshipDefinitionsBlock
+    {
+        public ImportRelationshipDefinitionsBlock(ICreateRelationshipDefinitionPipeline createRelationshipDefinitionPipeline, IEditRelationshiDefinitionPipeline editRelationshiDefinitionPipeline) : base(createRelationshipDefinitionPipeline, editRelationshiDefinitionPipeline)
+        {
+        }
+
+        protected override async Task ImportSingleEntity(ImportCatalogsArgument arg, CommercePipelineExecutionContext context, RelationshipDefinition entity)
+        {
+            Condition.Requires(arg, "arg").IsNotNull();
+            Condition.Requires(context, "context").IsNotNull();
+            Condition.Requires(entity, "entity").IsNotNull();
+            CreateRelationshipDefinitionArgument arg2 = new CreateRelationshipDefinitionArgument(entity.Name, string.IsNullOrWhiteSpace(entity.DisplayName) ? entity.Name : entity.DisplayName, string.IsNullOrWhiteSpace(entity.RelationshipDescription) ? entity.Name : entity.RelationshipDescription, entity.SourceType, entity.TargetType, entity.RenderList);
+            await CreateRelationshipDefinitionPipeline.Run(arg2, context).ConfigureAwait(continueOnCapturedContext: false);
+        }
+    }
+}
