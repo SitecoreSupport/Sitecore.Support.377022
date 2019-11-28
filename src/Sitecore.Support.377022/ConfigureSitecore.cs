0b58d19a81b31dbfaaa55c463d1aff653f2940c3
mao <mao@sitecore.net>
2019-11-28 10:55:42 +0200

    Replace default ImportRelationshipDefinitionsBlock

---
+++ src/Sitecore.Support.377022/ConfigureSitecore.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Framework.Configuration;
+using Sitecore.Framework.Pipelines.Definitions.Extensions;
+
+namespace Sitecore.Support
+{
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            services.Sitecore().Pipelines(config => config
+
+             .ConfigurePipeline<IImportCatalogsPipeline>(configure =>
+                configure.Replace<ImportRelationshipDefinitionsBlock, Sitecore.Support.Pipelines.Blocks.ImportRelationshipDefinitionsBlock>()));
+
+
+            services.RegisterAllCommands(assembly);
+        }
+    }
+}
